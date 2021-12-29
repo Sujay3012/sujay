9f8234c4f3b2b0c454afcd0696cee99c5b7b63aa
Sujay3012 <sujayjeff94@gmail>
2021-12-29 11:23:02 +0530

    message

---
+++ src/org/temp/Sub.java
@@ -0,0 +1,12 @@
+package org.temp;
+
+public class Sub {
+	private void rank() {
+	System.out.println("sujay");
+	}
+	public static void main(String[] args) {
+		Sub s = new Sub();
+		s.rank();
+	}
+
+}
